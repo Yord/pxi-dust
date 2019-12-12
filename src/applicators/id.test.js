90b98511c15e12a4aa0abf10994011408e40aa9a
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-12 23:35:32 +0100

    test: improve id applicator tests

--- src/applicators/id.test.js
+++ src/applicators/id.test.js
@@ -1,23 +1,24 @@
-const {array, assert, integer, jsonObject, property} = require('fast-check')
+const {anything, array, assert, integer, jsonObject, property} = require('fast-check')
 const {func: applicator} = require('./id')
 
 test('does not apply function, returns input unchanged as output', () => {
-  const verbose   = undefined
-  const failEarly = undefined
-  const fs        = undefined
-  const argv      = undefined
-  const apply     = applicator(verbose, failEarly, fs, argv)
-
-  const jsons     = array(jsonObject())
-  const lines     = integer()
+  const verbose   = anything()
+  const failEarly = anything()
+  const fs        = array(anything())
+  const argv      = anything()
+  const inputLen  = integer(0, 10)
 
   assert(
-    property(jsons, lines, (jsons, lines) =>
-      expect(
-        apply(jsons, lines)
-      ).toStrictEqual(
-        {err: '', jsons}
+    property(verbose, failEarly, fs, argv, inputLen, (verbose, failEarly, fs, argv, inputLen) => {
+      const jsons = array(jsonObject(), inputLen)
+      const lines = array(integer(), inputLen)
+      property(jsons, lines, (jsons, lines) =>
+        expect(
+          applicator(verbose, failEarly, fs, argv)(jsons, lines)
+        ).toStrictEqual(
+          {err: '', jsons}
+        )
       )
-    )
+    })
   )
 })
