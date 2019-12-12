ffdf1ef7f8a84c3eb286a0104e739e11a5fe89c7
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-12 23:35:42 +0100

    fix: improve id lexer tests

--- src/lexers/id.test.js
+++ src/lexers/id.test.js
@@ -1,19 +1,17 @@
-const {assert, integer, property, string} = require('fast-check')
+const {anything, assert, integer, property, string} = require('fast-check')
 const {func: lexer} = require('./id')
 
 test('passes on data as one big token, ignoring lines completely', () => {
-  const verbose   = undefined
-  const failEarly = undefined
-  const argv      = undefined
-  const lex       = lexer(verbose, failEarly, argv)
-
+  const verbose   = anything()
+  const failEarly = anything()
+  const argv      = anything()
   const data      = string()
   const prevLines = integer()
 
   assert(
     property(data, prevLines, (data, prevLines) =>
       expect(
-        lex(data, prevLines)
+        lexer(verbose, failEarly, argv)(data, prevLines)
       ).toStrictEqual(
         {err: '', tokens: [data], lines: [], lastLine: prevLines, rest: ''}
       )
