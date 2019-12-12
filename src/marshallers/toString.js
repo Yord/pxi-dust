018edf1a99a758a37900a2de9727f3c9897a4674
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-12 16:37:00 +0100

    fix: be aware of null and undefined in toString marshaller

--- src/marshallers/toString.js
+++ src/marshallers/toString.js
@@ -1,12 +1,12 @@
 module.exports = {
   name: 'toString',
   desc: 'applies Object.prototype.toString to the input and adds newlines.',
-  func: (verbose, failEarly, argv) => jsons => {
+  func: (verbose, failEarly, argv) => values => {
     let str = ''
 
-    for (let index = 0; index < jsons.length; index++) {
-      const obj = jsons[index]
-      str += obj.toString() + '\n'
+    for (let index = 0; index < values.length; index++) {
+      const value = values[index]
+      if (typeof value !== 'undefined' && value !== null) str += value.toString() + '\n'
     }
 
     return {err: '', str}
