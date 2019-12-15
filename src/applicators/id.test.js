94b404b861f346c739b511f547f1bc3f5ab52c1d
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-15 20:09:52 +0100

    chore: make err an array, and pass only argv

--- src/applicators/id.test.js
+++ src/applicators/id.test.js
@@ -1,22 +1,22 @@
-const {anything, array, assert, integer, jsonObject, property} = require('fast-check')
+const {anything, array, assert, constant, integer, jsonObject, property} = require('fast-check')
 const {func: applicator} = require('./id')
 
 test('does not apply function, returns input unchanged as output', () => {
-  const verbose   = anything()
-  const failEarly = anything()
-  const fs        = array(anything())
-  const argv      = anything()
-  const inputLen  = integer(0, 10)
+  const err      = []
+  const fs       = array(anything())
+  const verbose  = anything()
+  const argv     = verbose.map(verbose => constant({verbose}))
+  const inputLen = integer(0, 10)
 
   assert(
-    property(verbose, failEarly, fs, argv, inputLen, (verbose, failEarly, fs, argv, inputLen) => {
+    property(fs, argv, inputLen, (fs, argv, inputLen) => {
       const jsons = array(jsonObject(), inputLen)
       const lines = array(integer(), inputLen)
       property(jsons, lines, (jsons, lines) =>
         expect(
-          applicator(verbose, failEarly, fs, argv)(jsons, lines)
+          applicator(fs, argv)(jsons, lines)
         ).toStrictEqual(
-          {err: '', jsons}
+          {err, jsons}
         )
       )
     })
