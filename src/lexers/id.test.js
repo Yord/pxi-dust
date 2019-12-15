e0613cac8bbc7f0caeca6be7c13bdb37195dd197
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-15 20:10:12 +0100

    chore: make err an array, and pass only argv

--- src/lexers/id.test.js
+++ src/lexers/id.test.js
@@ -1,19 +1,19 @@
-const {anything, assert, integer, property, string} = require('fast-check')
+const {anything, assert, constant, integer, property, string} = require('fast-check')
 const {func: lexer} = require('./id')
 
 test('passes on data as one big token, ignoring lines completely', () => {
+  const err       = []
   const verbose   = anything()
-  const failEarly = anything()
-  const argv      = anything()
+  const argv      = verbose.map(verbose => constant({verbose}))
   const data      = string()
   const prevLines = integer()
 
   assert(
     property(data, prevLines, (data, prevLines) =>
       expect(
-        lexer(verbose, failEarly, argv)(data, prevLines)
+        lexer(argv)(data, prevLines)
       ).toStrictEqual(
-        {err: '', tokens: [data], lines: [], lastLine: prevLines, rest: ''}
+        {err, tokens: [data], lines: [], lastLine: prevLines, rest: ''}
       )
     )
   )
