f7d8d545513a13e056a0356fa5aad6133687a6ff
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-26 19:28:38 +0100

    chore: move id applicator to pf

--- src/applicators/id.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'id',
-  desc: 'does not apply any functions and returns the JSON objects unchanged.',
-  func: (fs, {}) => (jsons, lines) => (
-    {err: [], jsons: jsons}
-  )
-}
